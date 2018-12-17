77b1d33a941cf7e5310cd92bcdcfbd5124de1f67
Douglas Aparecido Vasconcelos <douglas_cpu@hotmail.com>
2018-12-17 14:11:29 -0200

    Initial Commit Signed-off-by: Douglas Aparecido Vasconcelos <douglas_cpu@hotmail.com>

---
+++ AssinadorSatA1Doug/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AssinadorSatA1Doug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAssinar = new System.Windows.Forms.Button();
+            this.txtAssinatura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoftwareHouse = new System.Windows.Forms.TextBox();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAssinar
+            // 
+            this.btnAssinar.Location = new System.Drawing.Point(424, 27);
+            this.btnAssinar.Name = "btnAssinar";
+            this.btnAssinar.Size = new System.Drawing.Size(82, 34);
+            this.btnAssinar.TabIndex = 0;
+            this.btnAssinar.Text = "Assinar";
+            this.btnAssinar.UseVisualStyleBackColor = true;
+            this.btnAssinar.Click += new System.EventHandler(this.btnAssinar_Click);
+            // 
+            // txtAssinatura
+            // 
+            this.txtAssinatura.Location = new System.Drawing.Point(12, 76);
+            this.txtAssinatura.Multiline = true;
+            this.txtAssinatura.Name = "txtAssinatura";
+            this.txtAssinatura.Size = new System.Drawing.Size(506, 121);
+            this.txtAssinatura.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CNPJ Software House:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CNPJ Cliente:";
+            // 
+            // txtSoftwareHouse
+            // 
+            this.txtSoftwareHouse.Location = new System.Drawing.Point(131, 24);
+            this.txtSoftwareHouse.Name = "txtSoftwareHouse";
+            this.txtSoftwareHouse.Size = new System.Drawing.Size(96, 20);
+            this.txtSoftwareHouse.TabIndex = 4;
+            this.txtSoftwareHouse.Text = "15288349000148";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(131, 50);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(96, 20);
+            this.txtCliente.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 212);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.txtSoftwareHouse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAssinatura);
+            this.Controls.Add(this.btnAssinar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Assinador Sat A1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAssinar;
+        private System.Windows.Forms.TextBox txtAssinatura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoftwareHouse;
+        private System.Windows.Forms.TextBox txtCliente;
+    }
+}
+
---
+++ AssinadorSatA1Doug/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace AssinadorSatA1Doug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAssinar_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                X509Store store = new X509Store(StoreLocation.CurrentUser);
+
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+                X509Certificate2Collection certCollection = (X509Certificate2Collection)store.Certificates;
+
+                X509Certificate2Collection foundCollection = (X509Certificate2Collection)certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+
+                X509Certificate2Collection selectedcollection = X509Certificate2UI.SelectFromCollection(foundCollection,
+
+                    "Select a Certificate.", "Select a Certificate from the following list to get information on that certificate", X509SelectionFlag.SingleSelection);
+
+
+
+                if (selectedcollection.Count > 0)
+
+                {
+
+                    X509Certificate2 cert = selectedcollection[0];
+
+
+
+                    string certificateData = "Subject: " + cert.Subject + Environment.NewLine + "IssuerName: " + cert.Issuer
+
+                            + "\nSerialNumber: " + cert.SerialNumber + "\nFriendlyName:\n" + cert.FriendlyName;
+
+
+
+                    MessageBox.Show(certificateData, "Certificate Data",
+
+                       MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+                    if (cert.Verify())
+
+                    {
+
+                        MessageBox.Show(cert.Subject + " is a valid certificate.", cert.FriendlyName,
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show(cert.Subject + " is not a valid certificate.", cert.FriendlyName,
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+
+
+                    RSACryptoServiceProvider rsaEncryptor = (RSACryptoServiceProvider)cert.PrivateKey;
+
+                    byte[] cipherData = rsaEncryptor.Encrypt(Encoding.UTF8.GetBytes(txtSoftwareHouse.Text + txtCliente.Text), true);
+
+                    txtAssinatura.Text = Convert.ToBase64String(cipherData);
+
+                }
+
+            }
+
+            catch (CryptographicException ex)
+
+            {
+
+                MessageBox.Show(ex.Message, ex.GetType().ToString(),
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message, ex.GetType().ToString(),
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
